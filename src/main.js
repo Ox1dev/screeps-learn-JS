8c4f177fc2db1edcdaada063dc439120a1f93db8
Ox1dev <kirill.coldline@gmail.com>
2025-11-05 02:55:10 -0800

    fix: return attack instructions for towers

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 const stage = require('@/stages/stageLvl')
 const roleManager = require('@/roles/roleManager')
 const buildBase = require('@/base/baseDeployment')
+const utils = require('@/utils')
 
 module.exports.loop = function () {
     let spawn = Game.spawns['Spawn1'];
     roleManager.run();
     stage.run(spawn);
     buildBase.run(spawn);
+    utils.run();
 }
